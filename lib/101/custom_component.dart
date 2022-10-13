c71363d58724505c9a4dbdedaeee7647280e50c7
harunlakodla <harunlakodla@gmail.com>
2022-10-13 20:57:05 +0300

    added custom food  button custom_component dart file

--- lib/101/custom_component.dart
+++ lib/101/custom_component.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 
-class Custom_Component extends StatelessWidget with _ColorUtility {
+class Custom_Component extends StatelessWidget{
   Custom_Component({Key? key}) : super(key: key);
 
   @override
@@ -9,12 +9,8 @@ class Custom_Component extends StatelessWidget with _ColorUtility {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(primary: white,shape:StadiumBorder() ),
-          onPressed: (){}, 
-          child: Text(
-            "Food",
-            style: Theme.of(context).textTheme.headline5?.copyWith(color: redColor),))),
+        child: CustomFoodButton(title: "Food",)
+            ),
     );
   } 
 }
@@ -22,4 +18,21 @@ class Custom_Component extends StatelessWidget with _ColorUtility {
 class _ColorUtility{
   final Color redColor=Colors.red;
   final Color white =Colors.white;
+}
+
+class CustomFoodButton extends StatelessWidget with _ColorUtility{
+  CustomFoodButton({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+          style: ElevatedButton.styleFrom(primary: white,shape:StadiumBorder() ),
+          onPressed: (){}, 
+          child: Text(
+            title.toString(),
+            style: Theme.of(context).textTheme.subtitle1?.copyWith(color: redColor),
+                )
+              );
+  }
 }
