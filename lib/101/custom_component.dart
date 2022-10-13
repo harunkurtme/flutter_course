afb555ee124aef4b5b8fb0bf5ae13433c756b8f7
harunlakodla <harunlakodla@gmail.com>
2022-10-13 20:52:43 +0300

    file update custom component

--- lib/101/custom_component.dart
+++ lib/101/custom_component.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 
-class Custom_Component extends StatelessWidget {
-  const Custom_Component({Key? key}) : super(key: key);
+class Custom_Component extends StatelessWidget with _ColorUtility {
+  Custom_Component({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(primary: white,shape:StadiumBorder() ),
+          onPressed: (){}, 
+          child: Text(
+            "Food",
+            style: Theme.of(context).textTheme.headline5?.copyWith(color: redColor),))),
+    );
+  } 
+}
+
+class _ColorUtility{
+  final Color redColor=Colors.red;
+  final Color white =Colors.white;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
-
-import '101/custom_component.dart';
+import 'package:flutter_course/101/custom_component.dart';
 
 void main() {
-  runApp(const Custom_Component());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Custom_Component(),
+    );
+  }
 }
