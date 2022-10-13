0b821c8988a1d5b2f1add6703a4be67b52da6b78
harunlakodla <harunlakodla@gmail.com>
2022-10-13 20:25:41 +0300

    first commit

---
+++ lib/101/custom_component.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Custom_Component extends StatelessWidget {
+  const Custom_Component({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
