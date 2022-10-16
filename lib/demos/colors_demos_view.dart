44e2607c05c0631deeb1c3fa1c81012cd4748a49
harunlakodla <harunlakodla@gmail.com>
2022-10-17 02:30:07 +0300

    volors demos view added backgroundupdate and selectionitem

--- lib/demos/colors_demos_view.dart
+++ lib/demos/colors_demos_view.dart
@@ -12,15 +12,54 @@ class ColorDemos extends StatefulWidget {
 class _ColorDemosState extends State<ColorDemos> {
   @override
   Widget build(BuildContext context) {
+
+    Color? background;
+    void backgroundUpdate(Color colorbackgorund){
+      setState(() {
+        
+      });
+    }
+
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Container(
-          child: Text("Color RED"),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (val){
+          if (val==_SelectionITem.red.index) {
+            
+          } else if(val ==_SelectionITem.green.index) {
+          }
+        },
+        backgroundColor: background,
+        items: [
+        BottomNavigationBarItem(icon: _ContainerColor(
           color: Colors.red,
         ),
         label: "Red"
-        )
+        ),BottomNavigationBarItem(icon: _ContainerColor(
+          color: Colors.blue,
+        ),
+        label: "Blue"
+        ),BottomNavigationBarItem(icon: _ContainerColor(
+          color: Colors.green,
+        ),
+        label: "Green"
+        ),
       ],),
+    );
+  }
+}
+
+enum _SelectionITem{
+  red,blue,green
+}
+
+class _ContainerColor extends StatelessWidget {
+  const _ContainerColor({Key? key, required this.color}) : super(key: key);
+
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
     );
   }
 }
