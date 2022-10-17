97a65b91c0602e44f95a32dda8b619dce7d260c9
harunlakodla <harunlakodla@gmail.com>
2022-10-17 21:57:06 +0300

    lifecycle file update for private funciton

--- lib/101/stateful_life_cycle.dart
+++ lib/101/stateful_life_cycle.dart
@@ -11,14 +11,59 @@ class StatefulLifeCycle extends StatefulWidget {
 }
 
 class _StatefulLifeCycleState extends State<StatefulLifeCycle> {
+
+  String _message="" ;
+  late bool _isOdd ;
+
+  //değişmeler olduğunda sayfa içinde kullanılır
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
+  //sayfadan çıkışta kullanılır
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  // sayfaya tekrar giriş yapılıp eski verileri değiştirmek için kullanılır
+  @override
+  void didUpdateWidget(covariant StatefulLifeCycle oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  //sayfaya girişte kullanılır
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  _isOdd= this.widget.title.length.isOdd;
+  _message=widget.title;
+
+  newMethod();
+
+  }
+
+  void newMethod() {
+    if (_isOdd) {
+     _message +="cift";
+    } else {
+      
+      _message +="tek";
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title.toString()),),
+      appBar: AppBar(title: Text(_message),),
       body: Center(
-        child: widget.title.length.isOdd ? ElevatedButton(onPressed: (){}, child: Text("")) : TextButton(onPressed: (){}, child: Text(""))
+        child: widget.title.length.isOdd ? ElevatedButton(onPressed: (){}, child: Text(_message)) : TextButton(onPressed: (){}, child: Text(_message)),
         ),
-      ),
+      
     );
   }
 }
