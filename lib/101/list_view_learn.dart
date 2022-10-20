19cec2719f2d44f11b11fb1f86d67ccb1c43f3b1
harunlakodla <harunlakodla@gmail.com>
2022-10-20 23:53:32 +0300

    list_view_learn.dart added file for listview and fittedbox etc.

---
+++ lib/101/list_view_learn.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ListViewLearn extends StatefulWidget {
+  ListViewLearn({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewLearn> createState() => _ListViewLearnState();
+}
+
+class _ListViewLearnState extends State<ListViewLearn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+
+
+        
+      ),
+    );
+  }
+}
