d5b77e5696f28c360a25db6451a7ec345b4d2c34
harunlakodla <harunlakodla@gmail.com>
2022-10-21 00:10:32 +0300

    list_view_learn changed files

--- lib/101/list_view_learn.dart
+++ lib/101/list_view_learn.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class ListViewLearn extends StatefulWidget {
@@ -12,9 +14,32 @@ class _ListViewLearnState extends State<ListViewLearn> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-
-
-        
+        children: [
+          Text("Merhaba",
+          style: Theme.of(context).textTheme.headline1,),
+          Container(
+            height: 300,
+            color: Colors.green,
+          ),
+          SizedBox(
+            height: 300,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  width: 100,
+                  color: Colors.blue,
+                ),Container(
+                  width: 100,
+                  color: Colors.blue,
+                ),Container(
+                  width: 100,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
